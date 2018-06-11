4c674e65f0f30d13d2cd2fb27a468354d29d45e2
christian <christiandavidphoto@gmail.com>
2018-06-11 13:19:05 -0400

    scrape img and title with cheerio

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
 const url = 'https://www.imdb.com/find?s=tt&ttype=ft&ref_=fn_ft&q=';
 
@@ -9,5 +10,21 @@ function searchMovies(searchTerm) {
 
 // make request and log out html
 searchMovies('star wars').then(body => {
-  console.log(body);
+  const movies = [];
+  const $ = cheerio.load(body);
+
+  $('.findResult').each(function(i, element) {
+    const $element = $(element);
+    const $image = $element.find('td a img');
+    const $title = $element.find('td.result_text a');
+
+    const movie = {
+      image: $image.attr('src'),
+      title: $title.text()
+    };
+
+    movies.push(movie);
+  });
+
+  console.log(movies);
 });
