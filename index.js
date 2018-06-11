8ab9a3345ce149a6f0625e9b3c1a5c137df5c37a
christian <christiandavidphoto@gmail.com>
2018-06-11 13:26:36 -0400

    express app setup

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+const express = require('express');
 
-const url = 'https://www.imdb.com/find?s=tt&ttype=ft&ref_=fn_ft&q=';
+const app = express();
 
-// fetch data from url
-function searchMovies(searchTerm) {
-  return fetch(`${url}${searchTerm}`).then(response => response.text());
-}
-
-// make request and log out html
-searchMovies('star wars').then(body => {
-  const movies = [];
-  const $ = cheerio.load(body);
-
-  $('.findResult').each(function(i, element) {
-    const $element = $(element);
-    const $image = $element.find('td a img');
-    const $title = $element.find('td.result_text a');
-
-    const movie = {
-      image: $image.attr('src'),
-      title: $title.text()
-    };
-
-    movies.push(movie);
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Scraping is fun!'
   });
+});
 
-  console.log(movies);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
 });
