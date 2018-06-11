b5a8d6791622ba16f5a4439d4d1069e023bbc204
christian <christiandavidphoto@gmail.com>
2018-06-11 13:02:00 -0400

    fetch function set up

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const fetch = require('node-fetch');
+
+const url = 'https://www.imdb.com/find?s=tt&ttype=ft&ref_=fn_ft&q=';
+
+// fetch data from url
+function searchMovies(searchTerm) {
+  return fetch(`${url}${searchTerm}`).then(response => response.text());
+}
+
+// make request and log out html
+searchMovies('star wars').then(body => {
+  console.log(body);
+});
